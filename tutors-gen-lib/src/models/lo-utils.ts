988148c956892e38101373f1640fead2335bbe4a
Eamonn de Leastar <edeleastar@wit.ie>
2025-06-12 17:55:53 +0100

    update lo-utils

--- tutors-gen-lib/src/models/lo-utils.ts
+++ tutors-gen-lib/src/models/lo-utils.ts
@@ -1,148 +1,95 @@
-import type { Calendar, Composite, Course, IconNav, Lo, LoType, Talk, Topic, WeekType } from "./lo-types.ts";
-
-export function filterByType(list: Lo[], type: LoType): Lo[] {
-  const los = flattenLos(list);
-  return los.filter((lo) => lo.type === type);
-}
-
-export function injectCourseUrl(lo: Lo, id: string, url: string) {
-  if (lo.type === "archive" || lo.type === "otherType") {
-    lo.route = lo.route?.replace("{{COURSEURL}}", url);
-  } else {
-    lo.route = lo.route?.replace("{{COURSEURL}}", id);
-  }
-
-  lo.img = lo.img?.replace("{{COURSEURL}}", url);
-  lo.video = lo.video?.replace("{{COURSEURL}}", id);
-  if (lo.type == "talk" || lo.type == "paneltalk") {
-    const talk = lo as Talk;
-    talk.pdf = talk.pdf?.replace("{{COURSEURL}}", url);
-  }
-  if ("los" in lo) {
-    // @ts-ignore
-    lo.los.forEach((childLo) => {
-      injectCourseUrl(childLo, id, url);
-    });
-  }
-}
-
-export function createToc(course: Course) {
-  course.los.forEach((lo) => {
-    const topic = lo as Topic;
-    topic.toc = [];
-    topic.toc.push(
-      ...topic.panels.panelVideos,
-      ...topic.panels.panelTalks,
-      ...topic.panels.panelNotes,
-      ...topic.units.units,
-      ...topic.units.standardLos,
-      ...topic.units.sides,
-    );
-
-    topic.toc.forEach((lo) => {
-      lo.parentLo = course;
-      lo.parentTopic = topic;
-      if (lo.type === "unit" || lo.type === "side") {
-        const composite = lo as Composite;
-        composite.los.forEach((subLo) => {
-          subLo.parentTopic = topic;
-        });
-      }
-    });
-  });
-}
-
-export function createCompanions(course: Course) {
-  course.companions = {
-    show: true,
-    bar: [],
-  };
-  const companionsList = [
-    {
-      key: "slack",
-      icon: "slack",
-      target: "_blank",
-      tip: "Go to module Slack channel",
-    },
-    {
-      key: "zoom",
-      icon: "zoom",
-      target: "_blank",
-      tip: "Go to module Zoom meeting",
-    },
-    {
-      key: "moodle",
-      icon: "moodle",
-      target: "_blank",
-      tip: "Go to module Moodle page",
-    },
-    {
-      key: "youtube",
-      icon: "youtube",
-      target: "_blank",
-      tip: "Go to module YouTube channel",
-    },
-    {
-      key: "teams",
-      icon: "teams",
-      target: "_blank",
-      tip: "Go to module Teams meeting",
-    },
-  ];
-  companionsList.forEach((companionItem) => {
-    const { key, icon, target, tip } = companionItem;
-    if (course.properties[key]) {
-      course.companions.bar.push({
-        link: course.properties[key],
-        icon,
-        target,
-        tip,
-      });
-    }
-  });
-  if (course.properties.companions) {
-    for (const [key, value] of Object.entries(course.properties.companions)) {
-      const companion: any = value;
-      course.companions.bar.push({
-        link: companion.link,
-        icon: key,
-        target: "_blank",
-        tip: companion.title,
-      });
-    }
-  }
-  course.companions.show = course.companions.bar.length > 0;
-}
-
-export function createWallBar(course: Course) {
-  course.wallBar = {
-    show: true,
-    bar: [],
-  };
-  course.walls?.forEach((wall) => {
-    course.wallBar.bar.push(createWallLink(wall[0].type, course));
-  });
-}
-
-function createWallLink(type: string, course: Course): IconNav {
-  return {
-    link: `/wall/${type}/${course.courseUrl}`,
-    icon: type,
-    tip: `${type}s`,
-    target: "",
-  };
-}
+import {
+  isCompositeLo,
+  type VideoIdentifier,
+  type Archive,
+  type Composite,
+  type IconType,
+  type Lab,
+  type Lo,
+  type LoType,
+  type PanelNote,
+  type Panels,
+  type PanelTalk,
+  type PanelVideo,
+  type Side,
+  type Talk,
+  type Unit,
+  type Units,
+} from "./lo-types.ts";
 
 export function flattenLos(los: Lo[]): Lo[] {
   let result: Lo[] = [];
   los.forEach((lo) => {
     result.push(lo);
     if ("los" in lo) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       result = result.concat(flattenLos(lo.los));
     }
   });
   return result;
+}
+
+export function filterByType(list: Lo[], type: LoType): Lo[] {
+  const los = flattenLos(list);
+  return los.filter((lo) => lo.type === type);
+}
+
+function filterLos<T>(los: Lo[], type: string): T[] {
+  const talks: T[] = [];
+  los.forEach((lo) => {
+    if (lo.type === type) talks.push(lo as T);
+  });
+  return talks;
+}
+
+export function fixRoutePaths(lo: Lo) {
+  if (lo.route && lo.route[0] === "#") {
+    lo.route = "/" + lo.route.slice(1);
+  }
+  if (lo.video && lo.video[0] === "#") {
+    lo.video = "/" + lo.video.slice(1);
+  }
+  if (lo.route.endsWith("md") && lo.video) {
+    lo.route = lo.video;
+  }
+}
+
+export function injectCourseUrl(los: Lo[], id: string, url: string) {
+  los.forEach((lo) => {
+    if (lo.type === "archive") {
+      const archive: Archive = lo as Archive;
+      archive.route = `https://${lo.route?.replace(
+        "/archive/{{COURSEURL}}",
+        url
+      )}/${archive.archiveFile}`;
+    } else {
+      lo.route = lo.route?.replace("{{COURSEURL}}", id);
+    }
+
+    lo.img = lo.img?.replace("{{COURSEURL}}", url);
+    lo.video = lo.video?.replace("{{COURSEURL}}", id);
+    if (lo.type == "talk" || lo.type == "paneltalk") {
+      const talk = lo as Talk;
+      talk.pdf = talk.pdf?.replace("{{COURSEURL}}", url);
+    }
+    if (lo.type == "lab") {
+      const lab = lo as Lab;
+      lab.pdf = lab.pdf?.replace("{{COURSEURL}}", url);
+    }
+
+    // legacy version of generator included hash based routes;
+    // remove these now:
+    fixRoutePaths(lo);
+  });
+}
+
+export function removeUnknownLos(los: Lo[]) {
+  los.forEach((lo, index) => {
+    if (lo.type === "unknown") {
+      los.splice(index, 1);
+    }
+  });
 }
 
 export function allVideoLos(los: Lo[]): Lo[] {
@@ -160,64 +107,96 @@ export function removeLeadingHashes(str: string): string {
   return hashIndex >= 0 ? str.substring(hashIndex + 1) : str;
 }
 
-export function removeFirstLine(str: string): string {
-  return str.split("\n").slice(1).join("\n");
-}
-
-export function fixRoutePaths(lo: Lo) {
-  if (lo.route && lo.route[0] === "#") {
-    lo.route = "/" + lo.route.slice(1);
-  }
-  if (lo.video && lo.video[0] === "#") {
-    lo.video = "/" + lo.video.slice(1);
-  }
-  if (lo.route.endsWith("md") && lo.video) {
-    lo.route = lo.video;
-  }
-}
-
-export function loadPropertyFlags(course: Course) {
-  course.isPortfolio = (course.properties?.portfolio as unknown as boolean) === true;
-  course.areVideosHidden = (course.properties?.hideVideos as unknown as boolean) === true;
-  course.areLabStepsAutoNumbered = (course.properties?.labStepsAutoNumber as unknown as boolean) === true;
-  course.authLevel = course.properties.auth as unknown as number;
-  course.hasEnrollment = false;
-  course.hasWhiteList = false;
-  course.ignorePin = course.properties?.ignorepin?.toString();
-}
-export function initCalendar(course: Course) {
-  const calendar = {
-    title: "unknown",
-    weeks: [],
+export function getPanels(los: Lo[]): Panels {
+  return {
+    panelVideos: filterLos<PanelVideo>(los, "panelvideo"),
+    panelTalks: filterLos<PanelTalk>(los, "paneltalk"),
+    panelNotes: filterLos<PanelNote>(los, "panelnote"),
   };
-  try {
-    if (course.calendar) {
-      const calendarObj = course.calendar;
-      calendar.title = calendarObj.title;
-      // @ts-ignore
-      calendar.weeks = calendarObj.weeks.map((weekObj: any) => ({
-        date: Object.keys(weekObj)[0],
-        title: weekObj[Object.keys(weekObj)[0]].title,
-        type: weekObj[Object.keys(weekObj)[0]].type,
-        dateObj: new Date(Object.keys(weekObj)[0]),
-      }));
+}
 
-      const today = Date.now();
+export function getUnits(los: Lo[]): Units {
+  let standardLos = los?.filter(
+    (lo) =>
+      lo.type !== "unit" &&
+      lo.type !== "panelvideo" &&
+      lo.type !== "paneltalk" &&
+      lo.type !== "panelnote" &&
+      lo.type !== "side"
+  );
+  standardLos = sortLos(standardLos);
+  return {
+    units: los?.filter((lo) => lo.type === "unit") as Unit[],
+    sides: los?.filter((lo) => lo.type === "side") as Side[],
+    standardLos: standardLos,
+  };
+}
+
+export function sortLos(los: Array<Lo>): Lo[] {
+  const orderedLos = los.filter((lo) => lo.frontMatter?.order);
+  const unOrderedLos = los.filter((lo) => !lo.frontMatter?.order);
+  orderedLos.sort(
+    (a: any, b: any) => a.frontMatter.order - b.frontMatter.order
+  );
+  return orderedLos.concat(unOrderedLos);
+}
+
+export function loadIcon(lo: Lo): IconType | undefined {
+  if (lo.frontMatter && lo.frontMatter.icon) {
+    return {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      const currentWeek = calendar.weeks.find(
-        (week, i) =>
-          today > Date.parse(week.date) &&
-          today <= Date.parse(calendar.weeks[i + 1]?.date),
-      );
+      type: lo.frontMatter.icon["type"],
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      course.courseCalendar = {
-        title: calendarObj.title,
-        weeks: calendar.weeks,
-        // @ts-ignore
-        currentWeek: currentWeek,
-      };
-    }
-  } catch (e) {
-    console.log("Error loading calendar");
+      color: lo.frontMatter.icon["color"],
+    };
   }
+  return undefined;
+}
+
+export function crumbs(lo: Lo | undefined, los: Lo[]) {
+  if (lo) {
+    if (lo.route.endsWith("/")) {
+      lo.route = lo.route.slice(0, -1);
+    }
+    crumbs(lo.parentLo, los);
+    los.push(lo);
+  }
+}
+
+export function setShowHide(lo: Lo, status: boolean) {
+  lo.hide = status;
+  if (isCompositeLo(lo)) {
+    const compositeLo = lo as Composite;
+    for (const childLo of compositeLo.los) {
+      //if (compositeLo.los) {
+      setShowHide(childLo, status);
+      // }
+    }
+  }
+}
+
+export function getVideoConfig(lo: Lo): VideoIdentifier {
+  const config: VideoIdentifier = { service: "youtube", id: "" };
+  if (lo.videoids?.videoIds?.length > 0) {
+    const lastVideo = lo.videoids.videoIds[lo.videoids.videoIds.length - 1];
+    if (lastVideo.service === "heanet" || lastVideo.service === "vimp") {
+      config.service = lastVideo.service;
+      config.id = lastVideo.id;
+    } else {
+      const parts = lo.video?.split("/") || [];
+      const id = parts.pop() || parts.pop() || "";
+      config.id = id;
+    }
+  }
+  if (config.service === "heanet") {
+    config.url = `https://media.heanet.ie/player/${config.id}`;
+  } else if (config.service === "vimp") {
+    config.url = `https://vimp.oth-regensburg.de/media/embed?key=${config.id}&autoplay=false&controls=true`;
+  } else if (config.service === "youtube") {
+    config.url = `https://www.youtube.com/embed/${config.id}`;
+    config.externalUrl = `https://www.youtube.com/watch?v=${config.id}`;
+  }
+  return config;
 }
