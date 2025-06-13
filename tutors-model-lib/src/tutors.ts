9890889be4bc4f5526e5cd35f7b75c6a56f61a11
Eamonn de Leastar <edeleastar@wit.ie>
2025-06-13 12:38:14 +0100

    include markdown in models

--- tutors-model-lib/src/tutors.ts
+++ tutors-model-lib/src/tutors.ts
@@ -4,3 +4,4 @@ export * from "./models/lo-utils.ts";
 export * from "./models/course-utils.ts";
 export * from "./models/lo-tree.ts";
 export * from "./models/search.ts";
+export * from "./models/markdown-utils.ts";
