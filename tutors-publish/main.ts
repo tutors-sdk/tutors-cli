2bc2af4ded56ec8210a4594b2dc101477f32095b
Eamonn de Leastar <edeleastar@wit.ie>
2025-06-14 07:49:19 +0100

    version updates

--- tutors-publish/main.ts
+++ tutors-publish/main.ts
@@ -8,8 +8,8 @@ import process from "node:process";
 function getVersionInfo(): string {
   const modelLibVersion = "0.0.6";
   const genLibVersion = "0.0.14";
-  const version = "0.0.5";
-    return `tutors models: \n- tutors-model-lib: ${modelLibVersion}\n- tutors-gen-lib: ${genLibVersion}\n- tutors-publish: ${version}`;
+  const version = "0.0.6";
+  return `tutors models: \n- tutors-model-lib: ${modelLibVersion}\n- tutors-gen-lib: ${genLibVersion}\n- tutors-publish: ${version}`;
 }
 
 if (!fs.existsSync("course.md")) {
