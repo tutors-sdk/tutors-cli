dc3e95ff4c7b3446c7b8fbf149b66ec8e4b37dfa
Eamonn de Leastar <edeleastar@wit.ie>
2025-06-16 09:52:25 +0100

    version 0.0.15

--- tutors-publish/main.ts
+++ tutors-publish/main.ts
@@ -5,13 +5,6 @@ import {
 import * as fs from "node:fs";
 import process from "node:process";
 
-function getVersionInfo(): string {
-  const modelLibVersion = "0.0.6";
-  const genLibVersion = "0.0.14";
-  const version = "0.0.6";
-  return `tutors models: \n- tutors-model-lib: ${modelLibVersion}\n- tutors-gen-lib: ${genLibVersion}\n- tutors-publish: ${version}`;
-}
-
 if (!fs.existsSync("course.md")) {
   console.log(
     "Cannot locate course.md. Please change to course folder and try again. "
@@ -23,4 +16,4 @@ if (!fs.existsSync("course.md")) {
   generateCourse(lo, destFolder);
 }
 
-console.log(getVersionInfo());
+console.log("Tutors Generator v0.0.15");
