10d153a30eaa8a810d0433c0f7f43ee46802afd8
Eamonn de Leastar <edeleastar@wit.ie>
2025-06-20 10:25:09 +0100

    incorporate npm version of publish

---
+++ tutors-publish-npm/src/tutors-publish.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import * as fs from "fs";
+import { parseCourse, generateCourse } from "@tutors/tutors-gen-lib";
+
+const versionStr = `tutors-publish: 0.0.18`;
+console.log(versionStr);
+
+if (!fs.existsSync("course.md")) {
+  console.log("Cannot locate course.md. Please Change to course folder and try again. ");
+} else {
+  const srcFolder = process.cwd();
+  const destFolder = `${srcFolder}/json`;
+  const lo = parseCourse(srcFolder);
+  generateCourse(lo, destFolder);
+}
+console.log(versionStr);
